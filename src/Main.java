9106b076b661dfa7fea5d92e2052b60a546ffffb
tjerd <tylerjerd9@gmail.com>
2019-11-20 00:31:56 -0500

    added all major functions

--- src/Main.java
+++ src/Main.java
@@ -1,13 +1,19 @@
+/*
+On my honor, I have neither given nor received
+unauthorized aid on this assignment
+ */
+
 import java.io.File;
 import java.io.FileWriter;
 import java.io.IOException;
 import java.io.PrintWriter;
+import java.lang.reflect.Array;
 import java.util.*;
 
 public class Main {
     public static int PC = 64;
     public static Map<Object, ArrayList<String>> map = new TreeMap<>();
-    public static int[] reg = {0,0,0,0,0,0,0,0,0,0,0,3,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0};
+    public static int[] reg = {0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0};
     public static int cycle = -1;
     public static int firstDataAddress = -1;
     //public static int location = -1;
@@ -68,7 +74,7 @@ public class Main {
 
     public static String cat1(String binary){
         String opcode = binary.substring(3,8);
-        String src1 = checkXZR(Integer.toString(toDec(binary.substring(8,13))));
+        String src1 = regToXZR(Integer.toString(toDec(binary.substring(8,13))));
         String offsetStr = Integer.toString(twosToDec(binary.substring(14)));
         String instruction = "";
 
@@ -84,7 +90,7 @@ public class Main {
         String ARM = instruction + " X" + src1 + ", " + "#" + offsetStr;
         final String command = instruction;
         final String instr_string = " X" + src1 + ", " + "#" + offsetStr;
-        map.put(PC, new ArrayList<String>(){{ add(command); add(instr_string); add("1"); add(src1); add(offsetStr); }});
+        map.put(PC, new ArrayList<String>(){{ add(command); add(instr_string); add("1"); add(XZRToReg(src1)); add(offsetStr); }});
         //System.out.println(binary + '\t' + PC + '\t' + ARM);
         return binary + '\t' + PC + '\t' + ARM;
     }
@@ -92,7 +98,7 @@ public class Main {
     public static String cat2(String binary){
         String opcode = binary.substring(3,10);
         String dest = Integer.toString(toDec(binary.substring(10,15)));
-        String src1 = checkXZR(Integer.toString(toDec(binary.substring(15,20))));
+        String src1 = regToXZR(Integer.toString(toDec(binary.substring(15,20))));
         String immVal = Integer.toString(twosToDec(binary.substring(20)));
         String instruction = "";
 
@@ -112,7 +118,7 @@ public class Main {
         String ARM = instruction + " X" + dest + ", X" + src1 + ", #" + immVal;
         final String command = instruction;
         final String instr_string =  " X" + dest + ", X" + src1 + ", #" + immVal;
-        map.put(PC, new ArrayList<String>(){{ add(command); add(instr_string); add("2"); add(dest); add(src1); add(immVal); }});
+        map.put(PC, new ArrayList<String>(){{ add(command); add(instr_string); add("2"); add(XZRToReg(dest)); add(XZRToReg(src1)); add(immVal); }});
         //System.out.println(binary + '\t' + PC + '\t' + ARM);
         return binary + '\t' + PC + '\t' + ARM;
     }
@@ -120,8 +126,8 @@ public class Main {
     public static String cat3(String binary){
         String opcode = binary.substring(3,11);
         String dest = Integer.toString(toDec(binary.substring(11,16)));
-        String src1 = checkXZR(Integer.toString(toDec(binary.substring(16,21))));
-        String src2 = checkXZR(Integer.toString(toDec(binary.substring(21,26))));
+        String src1 = regToXZR(Integer.toString(toDec(binary.substring(16,21))));
+        String src2 = regToXZR(Integer.toString(toDec(binary.substring(21,26))));
         String instruction = "";
 
         if (opcode .equals("10100000"))
@@ -144,7 +150,7 @@ public class Main {
         String ARM = instruction + " X" + dest + ", X" + src1 + ", X" + src2;
         final String command = instruction;
         final String instr_string = " X" + dest + ", X" + src1 + ", X" + src2;
-        map.put(PC, new ArrayList<String>(){{ add(command); add(instr_string); add("3"); add(dest); add(src1); add(src2); }});
+        map.put(PC, new ArrayList<String>(){{ add(command); add(instr_string); add("3"); add(XZRToReg(dest)); add(XZRToReg(src1)); add(XZRToReg(src2)); }});
         //System.out.println(binary + '\t' + PC + '\t' + ARM);
         return binary + '\t' + PC + '\t' + ARM;
     }
@@ -152,7 +158,7 @@ public class Main {
     public static String cat4(String binary){
         String opcode = binary.substring(3,11);
         String dest = Integer.toString(toDec(binary.substring(11,16)));
-        String src1 = checkXZR(Integer.toString(toDec(binary.substring(16,21))));
+        String src1 = regToXZR(Integer.toString(toDec(binary.substring(16,21))));
         String immVal = Integer.toString(twosToDec(binary.substring(21)));
         String instruction = "";
 
@@ -166,7 +172,7 @@ public class Main {
         String ARM = instruction + " X" + dest + ", [X" + src1 + ", #" + immVal + "]";
         final String command = instruction;
         final String instr_string = " X" + dest + ", [X" + src1 + ", #" + immVal + "]";
-        map.put(PC, new ArrayList<String>(){{ add(command); add(instr_string); add("4"); add(dest); add(src1); add(immVal); }});
+        map.put(PC, new ArrayList<String>(){{ add(command); add(instr_string); add("4"); add(XZRToReg(dest)); add(XZRToReg(src1)); add(immVal); }});
         //System.out.println(binary + '\t' + PC + '\t' + ARM);
         return binary + '\t' + PC + '\t' + ARM;
     }
@@ -185,24 +191,32 @@ public class Main {
 
     ////////////////////////////////// INSTRUCTIONS ////////////////////////////////////////////////
     public static ArrayList<String> simulation(){
-        cycle = 0;
+        cycle = 1;
         for (int i = 64; i < 64+map.size()*4; i += 4){   //64+map.size()*4
-            System.out.print(i + ": ");
-            System.out.println(map.get(i).get(0));
-            if (map.get(i).get(0) .equals("DUMMY"))
+            if (map.get(i).get(0) .equals("DUMMY")) {
+                printCycle(i, map.get(i));
                 break;
-            //location = i;
-
-            if (map.get(i).get(2) .equals("1")) {
-                // LOCATION IS CHANGED HERE
-                i = runCat1(i, map.get(i));      // returns new location -4 if branching to account for the loop
             }
-            else if (map.get(i).get(2) .equals("2")){}
-                //runCat2();
-            else if (map.get(i).get(2) .equals("3")){}
-                //runCat3();
-            else if (map.get(i).get(2) .equals("4")){}
-                //runCat4();
+            int newLocation = i;
+            String category = map.get(i).get(2);
+
+            if (category .equals("1")) {
+                // LOCATION IS CHANGED HERE
+                newLocation = runCat1(i, map.get(i));      // returns new location -4 if branching to account for the loop
+            }
+            else if (category .equals("2")) {
+                runCat2or3(map.get(i));
+            }
+            else if (category .equals("3")) {
+                runCat2or3(map.get(i));
+            }
+            else if (category .equals("4")) {
+                runCat4(map.get(i));
+            }
+
+            printCycle(i, map.get(i));
+            i = newLocation;
+            cycle++;
         }
 
         return new ArrayList<String>(){{add("HELLO WORLD");}};
@@ -245,6 +259,127 @@ public class Main {
             return true;
         else
             return false;
+    }
+
+//    public static void runCat2(ArrayList<String> list){
+//
+//    }
+
+    public static void runCat2or3(ArrayList<String> list){
+        String command = list.get(0);
+        int  dest = Integer.parseInt(list.get(3));
+        int src1 = Integer.parseInt(list.get(4));
+        int src2 = Integer.parseInt(list.get(5));
+        boolean isImmVal;
+        if (command.substring(command.length()-1) .equalsIgnoreCase("I")) {
+            isImmVal = true;
+        }
+        else
+            isImmVal = false;
+
+        if (command .equals("EOR") || command .equals("EORI")){
+            eor(isImmVal, dest, src1, src2);
+        }
+        else if (command .equals("ADD") || command .equals("ADDI")){
+            add(isImmVal, dest, src1, src2);
+        }
+        else if (command .equals("SUB") || command .equals("SUBI")){
+            sub(isImmVal, dest, src1, src2);
+        }
+        else if (command .equals("AND") || command .equals("ANDI")){
+            and(isImmVal, dest, src1, src2);
+        }
+        else if (command .equals("ORR") || command .equals("ORRI")){
+            orr(isImmVal, dest, src1, src2);
+        }
+        else if (command .equals("LSR")){
+            lsr(isImmVal, dest, src1, src2);
+        }
+        else if (command .equals("LSL")){
+            lsl(isImmVal, dest, src1, src2);
+        }
+    }
+
+    public static void eor(boolean isImmVal, int dest, int src1, int src2){
+        if (isImmVal)
+            reg[dest] = reg[src1] ^ src2;
+        else
+            reg[dest] = reg[src1] ^ reg[src2];
+    }
+
+    public static void add(boolean isImmVal, int dest, int src1, int src2){
+        if (isImmVal)
+            reg[dest] = reg[src1] + src2;
+        else
+            reg[dest] = reg[src1] + reg[src2];
+    }
+
+    public static void sub(boolean isImmVal, int dest, int src1, int src2){
+        if (isImmVal)
+            reg[dest] = reg[src1] - src2;
+        else
+            reg[dest] = reg[src1] - reg[src2];
+    }
+
+    public static void and(boolean isImmVal, int dest, int src1, int src2){
+        if (isImmVal)
+            reg[dest] = reg[src1] & src2;
+        else
+            reg[dest] = reg[src1] & reg[src2];
+    }
+
+    public static void orr(boolean isImmVal, int dest, int src1, int src2){
+        if (isImmVal)
+            reg[dest] = reg[src1] | src2;
+        else
+            reg[dest] = reg[src1] | reg[src2];
+    }
+
+    public static void lsr(boolean isImmVal, int dest, int src1, int src2){
+        if (isImmVal)
+            reg[dest] = reg[src1] >> src2;
+        else
+            reg[dest] = reg[src1] >> reg[src2];
+    }
+
+    public static void lsl(boolean isImmVal, int dest, int src1, int src2){
+        if (isImmVal)
+            reg[dest] = reg[src1] << src2;
+        else
+            reg[dest] = reg[src1] << reg[src2];
+    }
+
+    public static void runCat4(ArrayList<String> list){
+        String command = list.get(0);
+        String target = list.get(3);
+        String src1 = list.get(4);
+        String immVal = list.get(5);
+
+        if (command .equals("LDUR")) {
+            ldur(target, src1, immVal);
+        }
+        else if (command .equals("STUR")){
+            stur(target, src1, immVal);
+        }
+    }
+
+    public static void ldur(String dest, String src1, String immVal){
+        // Get value from memory stores at memory[valueOf(src1) + immVal]
+        // Then store value in dest
+        int srcIndex = Integer.parseInt(src1);  // register number
+        int offset = Integer.parseInt(immVal);
+        String value = map.get(reg[srcIndex]+offset).get(0);
+        int destIndex = Integer.parseInt(dest);
+        reg[destIndex] = Integer.parseInt(value);
+    }
+
+    public static void stur(String target, String src1, String immVal){
+        // Take the value in target and store it in memory[valueAt(src1) + immVal]
+        int tIndex = Integer.parseInt(target);
+        String value = Integer.toString(reg[tIndex]);
+        int srcIndex = Integer.parseInt(src1);
+        int offset = Integer.parseInt(immVal);
+        map.put(reg[srcIndex]+offset, new ArrayList<String>(){{ add(value); }});
     }
 
     ///////////////////////////////// HELPER FUNCTIONS ////////////////////////////////////////////
@@ -311,40 +446,31 @@ public class Main {
         return dec;
     }
 
-//    public static String decToTwosComp(int num){
-//        if (num >= 0)
-//            return Integer.toBinaryString(num);
-//
-//        String binary = Integer.toBinaryString(num);
-//        String sign = binary.substring(0,1);
-//        binary = binary.substring(1);
-//        String onesComp = sign;
-//        for (int i = 0; i < binary.length(); i++){
-//            if (binary.charAt(i) == '1')
-//                onesComp += '0';
-//            else
-//                onesComp += '1';
-//        }
-//        String twosComp = Integer.toBinaryString(toDec(onesComp) + 1);
-//        return twosComp;
-//    }
-
     // If the value for the register is 31, this function will return the "ZR" string instead of "31"
-    public static String checkXZR(String num){
+    public static String regToXZR(String num){
         if (num .equals("31") || num.equals("11111"))
             return "ZR";
         else
             return num;
     }
 
-    public static void printCycle(){
-        String instr_string = "0";
+    public static String XZRToReg(String num){
+        if (num .equals("ZR"))
+            return "31";
+        else
+            return num;
 
-        //int firstDataAddress = 124;
+    }
 
+    public static void printCycle(int instAddy, ArrayList<String> list){
+        String instr_string = "";
+        if (list.get(0) .equals("DUMMY"))
+            instr_string = list.get(0);
+        else
+            instr_string = list.get(0) + " " + list.get(1);
 
         System.out.println("--------------------");
-        System.out.println("Cycle " + cycle + ":" + '\t' + "temp" + '\t' + instr_string);
+        System.out.println("Cycle " + cycle + ":" + '\t' + instAddy + '\t' + instr_string);
         System.out.println();
         System.out.println("Registers");
         System.out.println("X00:" + '\t' + reg[0] + '\t' + reg[1] + '\t' + reg[2] + '\t' + reg[3] + '\t' + reg[4] +
